85363546bb8594366d869bc2e0b1990dfa46e84f
Waldemar Tomme <waldetomme@gmail.com>
2019-02-20 20:23:39 +0100

    Implemented Seek

--- OnTheGoPlayer/Helpers/BufferStream.cs
+++ OnTheGoPlayer/Helpers/BufferStream.cs
@@ -6,11 +6,17 @@ namespace OnTheGoPlayer.Helpers
 
     internal class BufferStream : Stream
     {
+        #region Private Fields
+
         private readonly Stream baseStream;
 
         private readonly byte[] buffer;
 
         private int currentMaxPosition = 0;
+
+        #endregion Private Fields
+
+        #region Public Constructors
 
         public BufferStream(Stream baseStream, int length)
         {
@@ -22,29 +28,27 @@ namespace OnTheGoPlayer.Helpers
             Length = length;
         }
 
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        public override bool CanRead { get; } = true;
+
+        public override bool CanSeek { get; } = true;
+
+        public override bool CanWrite { get; } = false;
+
+        public override long Length { get; }
+
+        public override long Position { get; set; }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
         public override void Flush()
         {
             throw new NotSupportedException();
-        }
-
-        public override long Seek(long offset, SeekOrigin origin)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void SetLength(long value)
-        {
-            throw new NotSupportedException();
-        }
-
-        private void EnsureData(long maxPosition)
-        {
-            if (maxPosition <= currentMaxPosition)
-                return;
-
-            var needCount = maxPosition - currentMaxPosition;
-            var readCount = baseStream.Read(buffer, currentMaxPosition, (int)needCount);
-            currentMaxPosition += readCount;
         }
 
         public override int Read(byte[] buffer, int offset, int count)
@@ -58,19 +62,52 @@ namespace OnTheGoPlayer.Helpers
             return (int)realCount;
         }
 
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            var newPos = Position + offset;
+            switch (origin)
+            {
+                case SeekOrigin.Begin:
+                    newPos = offset;
+                    break;
+
+                case SeekOrigin.End:
+                    newPos = Length + offset;
+                    break;
+            }
+
+            if (newPos > Length)
+                throw new NotSupportedException("Cannot seek behind the stream.");
+
+            EnsureData(newPos);
+            Position = newPos;
+            return Position;
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+
         public override void Write(byte[] buffer, int offset, int count)
         {
             throw new NotSupportedException();
         }
 
-        public override bool CanRead { get; } = true;
+        #endregion Public Methods
 
-        public override bool CanSeek { get; } = true;
+        #region Private Methods
 
-        public override bool CanWrite { get; } = false;
+        private void EnsureData(long maxPosition)
+        {
+            if (maxPosition <= currentMaxPosition)
+                return;
 
-        public override long Length { get; }
+            var needCount = maxPosition - currentMaxPosition;
+            var readCount = baseStream.Read(buffer, currentMaxPosition, (int)needCount);
+            currentMaxPosition += readCount;
+        }
 
-        public override long Position { get; set; }
+        #endregion Private Methods
     }
 }
