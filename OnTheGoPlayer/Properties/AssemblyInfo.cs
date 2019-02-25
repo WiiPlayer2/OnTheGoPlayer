191ff96bca1c1866282018ffefd147a92a830c27
Waldemar Tomme <waldetomme@gmail.com>
2019-02-25 13:30:46 +0100

    Added webpublish settings

--- OnTheGoPlayer/Properties/AssemblyInfo.cs
+++ OnTheGoPlayer/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OnTheGoPlayer")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © DarkLink 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -50,7 +50,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: InternalsVisibleTo("OnTheGoPlayer.Test")]
 [assembly: ConfigureAwait(false)]
