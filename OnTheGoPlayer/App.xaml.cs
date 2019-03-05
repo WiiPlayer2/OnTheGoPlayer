d9890bbcc522dbcb181e00d99251f92f54ccf810
Waldemar Tomme <waldetomme@gmail.com>
2019-03-05 09:19:50 +0100

    Added single instance checking

--- OnTheGoPlayer/App.xaml.cs
+++ OnTheGoPlayer/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading;
 using System.Windows;
 
 namespace OnTheGoPlayer
@@ -13,5 +8,16 @@ namespace OnTheGoPlayer
     /// </summary>
     public partial class App : Application
     {
+        private const string APP_GUID = "9d598cdb-8fd5-4828-a544-56a8a5fe4b60";
+        private Mutex singleInstanceMutex;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            singleInstanceMutex = new Mutex(true, $"Local\\{APP_GUID}", out var createdNew);
+            if (!createdNew)
+                Shutdown();
+            else
+                base.OnStartup(e);
+        }
     }
 }
