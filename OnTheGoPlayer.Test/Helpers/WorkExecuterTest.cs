11d9a8084eb7bdf8cd8ca0b943fe52190c4423a9
Waldemar Tomme <waldetomme@gmail.com>
2019-05-10 07:20:51 +0200

    Added WorkExecuter and simple unit tests

---
+++ OnTheGoPlayer.Test/Helpers/WorkExecuterTest.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using FluentAssertions.Extensions;
+using NUnit.Framework;
+using OnTheGoPlayer.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OnTheGoPlayer.Test.Helpers
+{
+    [TestFixture, Timeout(10000)]
+    public class WorkExecuterTest
+    {
+        #region Public Methods
+
+        [Test]
+        public void Execute_WithAnyAction_ShouldReturnImmediately()
+        {
+            var executer = new WorkExecuter();
+
+            Action act = () => executer.Execute(() => Thread.Sleep(5000));
+
+            act.ExecutionTime().Should().BeLessThan(1.Seconds());
+        }
+
+        [Test]
+        public async void Execute_WithAnyAction_ShouldSetIsWorking()
+        {
+            var executer = new WorkExecuter();
+
+            var task = executer.Execute(() => Thread.Sleep(5000));
+
+            executer.IsWorking.Should().BeTrue();
+
+            await task;
+
+            executer.IsWorking.Should().BeFalse();
+        }
+
+        [Test]
+        public void Execute_WithAnyAsyncAction_ShouldReturnImmediately()
+        {
+            var executer = new WorkExecuter();
+
+            Action act = () => executer.Execute(async () => await Task.Delay(5000));
+
+            act.ExecutionTime().Should().BeLessThan(1.Seconds());
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ OnTheGoPlayer/Helpers/WorkExecuter.cs
@@ -0,0 +1,48 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnTheGoPlayer.Helpers
+{
+    public class WorkExecuter : INotifyPropertyChanged
+    {
+        #region Public Constructors
+
+        public WorkExecuter()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion Public Constructors
+
+        #region Public Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion Public Events
+
+        #region Public Properties
+
+        public bool IsWorking { get; private set; }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        public Task Execute(Action action, Action<Exception> onException = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Execute(Func<Task> asyncAction, Action<Exception> onException = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion Public Methods
+    }
+}
